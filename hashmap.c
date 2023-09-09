f79ee9af4c02b56e1b057905bd15b9b545657ba7
Replit user <>
2023-09-09 20:44:21 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -104,7 +104,7 @@ void eraseMap(HashMap * map,  char * key) {
 // Para hacerlo debe buscar el dato y luego marcarlo para que no sea válido.
   int position = hash(key, map->capacity);
   while (map->buckets[position]!=NULL){
-    if (map->bucket[position]->key !=NULL && strcmp(map->buckets[position]->key, key)==0){
+    if (map->buckets[position]->key !=NULL && strcmp(map->buckets[position]->key, key)==0){
       map->buckets[position]->key=NULL;
       map->size--;
       return;
